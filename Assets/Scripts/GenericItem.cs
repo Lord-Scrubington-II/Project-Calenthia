75267e80f074b0ce7708817f98faf078442faa34
Manit Soni <manitnsoni@gmail.com>
2020-10-24 15:45:48 -0700

    added preliminary Inventory and started GenericItem

--- Assets/Scripts/GenericItem.cs
+++ Assets/Scripts/GenericItem.cs
@@ -1,3 +1,14 @@
-﻿public class GenericItem
-{
+﻿public class GenericItem {
+
+    public ItemType itemType;
+    public int amount;
+
+    // random items; subject to change
+    // conenct each enum to a sprite to keep track
+    public enum ItemType {
+        HealthPotion,
+        ManaPotion,
+        Money
+    }
+
 }
---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory {
+    private List<GenericItem> itemList;
+
+    public Inventory()
+    {
+        itemList = new List<GenericItem>();
+    }
+
+    // Getter for class.
+    public List<GenericItem> GetItemList()
+    {
+        return itemList;
+    }
+
+    // Adds an item to the inventory.
+    public void AddItem(GenericItem item)
+    {
+        itemList.Add(item);
+    }
+
+    public void RemoveItem(GenericItem item)
+    {
+        itemList.Remove(item);
+    }
+
+}

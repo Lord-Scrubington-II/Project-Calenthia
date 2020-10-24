ef82146dc926faaaf652385ca3e800c83bd138bc
Zane Wang <zane.z.wang@gmail.com>
2020-10-24 16:39:19 -0700

    Skeleton Code for Combat Manager, PartyData

---
+++ Assets/Scripts/CombatManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatManager : MonoBehaviour
+{
+    GenericActor[] battleParticipants;
+    GenericActor[] playerParty;
+    GenericActor[] enemyFormation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

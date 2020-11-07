9b77dfd85ffb4359f1fac190f79a597b90bb032e
Lord-Scrubington-II (Zane W.) <zane.z.wang@gmail.com>
2020-11-06 21:06:43 -0800

    Update Mage.cs

--- Assets/Scripts/Character Classes/Mage.cs
+++ Assets/Scripts/Character Classes/Mage.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mage : MonoBehaviour
+public class Mage : PlayerActor
 {
     // Start is called before the first frame update
     void Start()

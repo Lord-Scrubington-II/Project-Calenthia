1f49a466ead958ab4351a9213175037554172caf
Lord-Scrubington-II (Zane W.) <zane.z.wang@gmail.com>
2020-11-12 00:56:41 -0800

    Never Mind

--- Assets/Scripts/Battle Elements/PlayerActor.cs
+++ Assets/Scripts/Battle Elements/PlayerActor.cs
@@ -2,86 +2,83 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace BattleElements
+public abstract class PlayerActor : GenericActor
 {
-    public abstract class PlayerActor : GenericActor
+
+    GenericItem weapon;
+    GenericItem armour;
+    protected bool isMartial;
+
+    public GrowthRateMatrix AllGrowthRates { get => growthRates; protected set => growthRates = value; }
+
+    [System.Serializable]
+    public struct GrowthRateMatrix
+    {
+        [SerializeField] private uint hp;
+        [SerializeField] private uint mp;
+        [SerializeField] private uint attack;
+        [SerializeField] private uint mAttack;
+        [SerializeField] private uint defense;
+        [SerializeField] private uint mDefense;
+        [SerializeField] private uint speed;
+        [SerializeField] private uint luck;
+
+
+        public GrowthRateMatrix(uint HP, uint MP, uint atk, uint mAtk, uint def, uint mDef, uint spd, uint luk)
+        {
+            hp = HP;
+            mp = MP;
+            attack = atk;
+            mAttack = mAtk;
+            defense = def;
+            mDefense = mDef;
+            speed = spd;
+            luck = luk;
+        }
+
+
+        public uint AttackGrowth { get => attack; set => attack = value; }
+        public uint MAttackGrowth { get => mAttack; set => attack = value; }
+        public uint DefenseGrowth { get => defense; set => attack = value; }
+        public uint MDefenseGrowth { get => mDefense; set => attack = value; }
+        public uint SpeedGrowth { get => speed; set => attack = value; }
+        public uint LuckGrowth { get => luck; set => attack = value; }
+        public uint HPGrowth { get => hp; set => hp = value; }
+        public uint MPGrowth { get => mp; set => mp = value; }
+    }
+
+    [SerializeField] private GrowthRateMatrix growthRates;
+
+    public override void Flee()
+    {
+        //flee based on speed of caller, 
+        //base chance to flee is determined by differential between level of invoker and level of enemy.
+        //invoke scenemanager if the flee check passes
+    }
+
+    /*
+    public override void UseSkill(GenericSkill skill)
     {
 
-        GenericItem weapon;
-        GenericItem armour;
-        protected bool isMartial;
-
-        public GrowthRateMatrix AllGrowthRates { get => growthRates; protected set => growthRates = value; }
-
-        [System.Serializable]
-        public struct GrowthRateMatrix
-        {
-            [SerializeField] private uint hp;
-            [SerializeField] private uint mp;
-            [SerializeField] private uint attack;
-            [SerializeField] private uint mAttack;
-            [SerializeField] private uint defense;
-            [SerializeField] private uint mDefense;
-            [SerializeField] private uint speed;
-            [SerializeField] private uint luck;
-
-
-            public GrowthRateMatrix(uint HP, uint MP, uint atk, uint mAtk, uint def, uint mDef, uint spd, uint luk)
-            {
-                hp = HP;
-                mp = MP;
-                attack = atk;
-                mAttack = mAtk;
-                defense = def;
-                mDefense = mDef;
-                speed = spd;
-                luck = luk;
-            }
-
-
-            public uint AttackGrowth { get => attack; set => attack = value; }
-            public uint MAttackGrowth { get => mAttack; set => attack = value; }
-            public uint DefenseGrowth { get => defense; set => attack = value; }
-            public uint MDefenseGrowth { get => mDefense; set => attack = value; }
-            public uint SpeedGrowth { get => speed; set => attack = value; }
-            public uint LuckGrowth { get => luck; set => attack = value; }
-            public uint HPGrowth { get => hp; set => hp = value; }
-            public uint MPGrowth { get => mp; set => mp = value; }
-        }
-
-        [SerializeField] private GrowthRateMatrix growthRates;
-
-        public override void Flee()
-        {
-            //flee based on speed of caller, 
-            //base chance to flee is determined by differential between level of invoker and level of enemy.
-            //invoke scenemanager if the flee check passes
-        }
-
-        /*
-        public override void UseSkill(GenericSkill skill)
-        {
-
-        }
-        */
-
-        public void LevelUp()
-        {
-            uint HPMaxDelta = AllGrowthRates.HPGrowth;
-            uint MPMaxDelta = AllGrowthRates.MPGrowth;
-            uint ATKDelta = AllGrowthRates.AttackGrowth;
-            uint MATKDelta = AllGrowthRates.MAttackGrowth;
-            uint DEFDelta = AllGrowthRates.DefenseGrowth;
-            uint MDEFDelta = AllGrowthRates.MDefenseGrowth;
-            uint SPDDelta = AllGrowthRates.SpeedGrowth;
-            uint LUKDelta = AllGrowthRates.LuckGrowth;
-
-            this.AllStats.ModAllStats(ATKDelta, MATKDelta, DEFDelta, MDEFDelta, SPDDelta, LUKDelta);
-            this.HPMax = HPMaxDelta;
-            this.MPMax = MPMaxDelta;
-            this.Level++;
-        }
-
-        public abstract void LoadLevelOneStats();
     }
+    */
+
+    public void LevelUp()
+    {
+        uint HPMaxDelta = AllGrowthRates.HPGrowth;
+        uint MPMaxDelta = AllGrowthRates.MPGrowth;
+        uint ATKDelta = AllGrowthRates.AttackGrowth;
+        uint MATKDelta = AllGrowthRates.MAttackGrowth;
+        uint DEFDelta = AllGrowthRates.DefenseGrowth;
+        uint MDEFDelta = AllGrowthRates.MDefenseGrowth;
+        uint SPDDelta = AllGrowthRates.SpeedGrowth;
+        uint LUKDelta = AllGrowthRates.LuckGrowth;
+
+        this.AllStats.ModAllStats(ATKDelta, MATKDelta, DEFDelta, MDEFDelta, SPDDelta, LUKDelta);
+        this.HPMax = HPMaxDelta;
+        this.MPMax = MPMaxDelta;
+        this.Level++;
+    }
+
+    public abstract void LoadLevelOneStats();
 }
--- Assets/Scripts/Party Data/PartyData.cs
+++ Assets/Scripts/Party Data/PartyData.cs
@@ -3,30 +3,28 @@ using System.Collections.Generic;
 using System.Net.Http.Headers;
 using UnityEngine;
 
-namespace BattleElements
+
+public static class PartyData
 {
-    public static class PartyData
+    static PlayerActor[] playerParty = new PlayerActor[4];
+    static PlayerActor[] playerFrontline = new PlayerActor[3];
+    static PlayerActor[] playerBackline = new PlayerActor[3];
+    static PlayerActor leader;
+    static Sprite leaderSprite;
+    static uint money;
+    static List<GenericItem> inventory;
+
+    public static PlayerActor[] PlayerParty { get => playerParty; set => playerParty = value; }
+    public static PlayerActor Leader { get => leader; set => leader = value; }
+    public static Sprite LeaderSprite { get => leaderSprite; }
+    public static uint Money { get => money; set => money = value; }
+    public static List<GenericItem> Inventory { get => inventory; }
+    public static PlayerActor[] PlayerFrontline { get => playerFrontline; set => playerFrontline = value; }
+    public static PlayerActor[] PlayerBackline { get => playerBackline; set => playerBackline = value; }
+
+    static GenericItem useItem(GenericItem item)
     {
-        static PlayerActor[] playerParty = new PlayerActor[4];
-        static PlayerActor[] playerFrontline = new PlayerActor[3];
-        static PlayerActor[] playerBackline = new PlayerActor[3];
-        static PlayerActor leader;
-        static Sprite leaderSprite;
-        static uint money;
-        static List<GenericItem> inventory;
-
-        public static PlayerActor[] PlayerParty { get => playerParty; set => playerParty = value; }
-        public static PlayerActor Leader { get => leader; set => leader = value; }
-        public static Sprite LeaderSprite { get => leaderSprite; }
-        public static uint Money { get => money; set => money = value; }
-        public static List<GenericItem> Inventory { get => inventory; }
-        public static PlayerActor[] PlayerFrontline { get => playerFrontline; set => playerFrontline = value; }
-        public static PlayerActor[] PlayerBackline { get => playerBackline; set => playerBackline = value; }
-
-        static GenericItem useItem(GenericItem item)
-        {
-            return null;
-        }
-
+        return null;
     }
+
 }

5597049c574eb31a0a48d9aed5f64c72396d7638
bannedfromikea <74336052+bannedfromikea@users.noreply.github.com>
2020-11-14 20:37:43 -0800

    Made Rage

---
+++ Assets/Scripts/Buff and Debuff/Rage.cs
@@ -0,0 +1,27 @@
+using system; 
+
+public class Rage:BuffDebuff 
+{
+
+    //Constructor initializing fields 
+    public Rage(int dur, GenericActor a)
+    {
+        this.duration = dur;
+        this.actor = a;
+    }
+
+    //double attack half defense 
+    //param a: The actor to which this will be applied
+    public override int execute(GenericActor a)
+    {
+        ChangeMod.changeAtk(a, 100);
+        ChangeMod.changeDef(a, -50);
+        ChangeMod.changeMDef(a, -50); 
+        this.duration--;
+        return this.duration;
+    }
+}
+
+
+
+

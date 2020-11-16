1b5ac2e80e4c04bef40f0ea1ea98c01b0a3062f3
J <jonasenchang@gmail.com>
2020-11-16 04:26:30 -0800

    scuffed main menu

---
+++ Assets/Scripts/Menu UI/ScrollingBG.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBG : MonoBehaviour
+{
+    public float speed;
+    public Renderer bgRender;
+
+    // Update is called once per frame
+    void Update()
+    {
+        bgRender.material.mainTextureOffset += new Vector2(speed * Time.deltaTime, 0f);
+    }
+}

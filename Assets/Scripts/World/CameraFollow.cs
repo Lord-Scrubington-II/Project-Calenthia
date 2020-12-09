7b2b02b419692ad1b4c3020cffba0949057dc57a
tino <vovthuy@gmail.com>
2020-12-09 13:42:17 -0800

    area transition

--- Assets/Scripts/World/CameraFollow.cs
+++ Assets/Scripts/World/CameraFollow.cs
@@ -10,6 +10,7 @@ namespace world {
     public class CameraFollow : MonoBehaviour {
 
         private float xOff, yOff, zOff;
+        private float smoothing = .1f;
 
         //player's position
         private Transform playerTransform;
@@ -28,16 +29,18 @@ namespace world {
 		private void LateUpdate() {
             if (transform.position != playerTransform.position) {
 
-                Vector3 temp = transform.position;
-                temp.x = playerTransform.position.x + xOff;
-                temp.y = playerTransform.position.y + yOff;
-                temp.z = playerTransform.position.z + zOff;
-
+                //Vector3 temp = transform.position;
+                Vector3 temp = new Vector3(playerTransform.position.x + xOff,
+                    playerTransform.position.y + yOff,
+                    playerTransform.position.z + zOff);
+                
                 //bounding the coordinates
                 temp.x = Mathf.Clamp(temp.x, minPos.x, maxPos.x);
                 temp.y = Mathf.Clamp(temp.y, minPos.y, maxPos.y);
 
-                transform.position = temp;
+                //transform.position = temp;
+                transform.position = Vector3.Lerp(transform.position,
+                    temp, smoothing);
             }
         }
     }
---
+++ Assets/Scripts/World/RoomTransfer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using world;
+
+namespace world {
+    public class RoomTransfer : MonoBehaviour {
+        public Vector2 minChange;
+        public Vector2 maxChange;
+        public Vector3 playerChange;
+        private CameraFollow camFollow;
+
+        // Start is called before the first frame update
+        void Start() {
+            camFollow = Camera.main.GetComponent<CameraFollow>();
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision) {
+            if (collision.CompareTag("Player")) {
+				camFollow.minPos = minChange;
+                camFollow.maxPos = maxChange;
+                collision.transform.position += playerChange;
+            }
+        }
+    }
+}
